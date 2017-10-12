b9313f922e585c471e3162b455d5a9538c58b3d3
Paul Irish <paul.irish@gmail.com>
2017-10-12 13:43:35 -0700

    max width of 80

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,7 @@ module.exports = {
     'body-tense': [1, 'always', ['present-imperative']],
     'footer-leading-blank': [1, 'always'],
     'footer-tense': [1, 'always', ['present-imperative']],
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 80],
     lang: [0, 'always', 'eng'],
     'scope-case': [2, 'always', 'lowerCase'],
     'scope-empty': [0, 'never'],
